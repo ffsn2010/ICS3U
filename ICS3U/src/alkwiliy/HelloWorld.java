d8cd276f5c660fa39306665f8ad6d19bbb7dcdb0
323369397 <323369397@C-3ZNGYR1.tdsb.on.ca>
2016-03-02 14:44:45 -0500

    Created a Hello World File that displayed "Hello World" when ran.

---
+++ ICS3U/src/alkwiliy/HelloWorld.java
@@ -0,0 +1,14 @@
+package alkwiliy;
+/*
+ * HelloWorld.java
+ * The program displays Hello World
+ * Faisal Al-kwiliy
+ * Wednedsay, March 3, 2016
+ */
+public class HelloWorld {
+
+	public static void main(String[] args){
+	//This prints hello world to the console
+			System.out.println("Hello World");
+	}
+}
