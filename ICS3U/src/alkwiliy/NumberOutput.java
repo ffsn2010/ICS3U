7a8f10d0f4c08abe6e005122b8b857cc08fda127
Faisal Al-kwiliy <faisal.al-kwiliy@hotmail.com>
2016-03-07 14:54:35 -0500

    Number Output Program

---
+++ ICS3U/src/alkwiliy/NumberOutput.java
@@ -0,0 +1,21 @@
+package alkwiliy;
+/*
+ * NumberOutput.java
+ * The program displays numbers
+ * Faisal Al-kwiliy
+ * Monday, March 7, 2016
+ */
+public class NumberOutput {
+	public static void main(String[] args){
+		System.out.println(489);
+		System.out.println("0");
+		System.out.println(-44);
+		System.out.println("4 + 3");
+		System.out.println(4+3);
+		System.out.println("38-9+25");
+		System.out.println(38-9+25);
+		System.out.println("26" + "18");
+		System.out.println("26" + 18);
+		System.out.println(15 + "31");
+	}
+}
