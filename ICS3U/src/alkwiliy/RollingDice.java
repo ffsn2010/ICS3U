6a2bac800cc031efd94395fcf890d187cda6b7d0
Faisal Al-kwiliy <faisal.al-kwiliy@hotmail.com>
2016-03-29 14:51:55 -0400

    The program stimulates rolling two dice at random for the user and the computer, and tells the result of who won, or if it was a tie. It also asks the user if he/she wants to play again.

--- ICS3U/src/alkwiliy/RollingDice.java
+++ ICS3U/src/alkwiliy/RollingDice.java
@@ -1,7 +1,10 @@
 package alkwiliy;
+
+import java.util.Scanner;
+
 /**
  * RollingDice.java
- * The program stimulates rolling two dice at random for the user, and then for the computer, and announcing who won between the computer and the user, or say it was a tie.                                                                                                                                                                     
+ * The program stimulates rolling two dice at random for the user, and then for the computer, and announcing who won between the computer and the user, or say it was a tie and lets the user plays until he wants to stop.                                                                                                                                                                     
  * Faisal Al-kwiliy
  *Wednesday, March 23, 2016
  **/
@@ -16,38 +19,50 @@ public class RollingDice {
 		 * @param args
 		 */
 
-		//*** variables
-		int UserDie1;
-		int UserDie2;
-		int ComputerDie1;
-		int ComputerDie2;
-		int UserTotalRoll;
-		int ComputerTotalRoll;
-		
-		//*** processing
-		UserDie1= (int)(Math.random()*6)+1;
-		UserDie2= (int)(Math.random()*6)+1;
-		ComputerDie1= (int)(Math.random()*6)+1;
-		ComputerDie2= (int)(Math.random()*6)+1;
-		
-		UserTotalRoll = UserDie1 + UserDie2;
-		ComputerTotalRoll = ComputerDie1 + ComputerDie2;
+		Scanner scan = new Scanner(System.in);
 
-		System.out.println("User's roll: " + UserTotalRoll);
-		System.out.println("Computer's roll: " + ComputerTotalRoll);
-		
+		//*** variables
+		int userDie1;
+		int userDie2;
+		int computerDie1;
+		int computerDie2;
+		int userTotalRoll;
+		int computerTotalRoll;
+		String playAgain = "yes";
+
 		//*** output
-		if (UserTotalRoll>ComputerTotalRoll){
-			System.out.println("Congratulations, the user won");
-		}
-		else if (UserTotalRoll<ComputerTotalRoll){
-			System.out.println("Sorry, the computer won");
-		}
-		else {
-			System.out.println("The user and computer are tied.");
+
+		while(playAgain.equalsIgnoreCase("yes")){
+
+			userDie1= (int)(Math.random()*6)+1;
+			userDie2= (int)(Math.random()*6)+1;
+			computerDie1= (int)(Math.random()*6)+1;
+			computerDie2= (int)(Math.random()*6)+1;
+
+			userTotalRoll = userDie1 + userDie2;
+			computerTotalRoll = computerDie1 + computerDie2;
+
+
+			System.out.println("User's roll: " + userTotalRoll);
+			System.out.println("Computer's roll: " + computerTotalRoll);
+			if (userTotalRoll>computerTotalRoll){
+				System.out.println("Congratulations, the user won");
+			}
+			else if (userTotalRoll<computerTotalRoll){
+				System.out.println("Sorry, the computer won");
+			}
+			else {
+				System.out.println("The user and computer are tied.");
+			}
+
+			System.out.println("Play again?");
+			playAgain = scan.nextLine();
+
 		}
 
 	}
 
 
+
 }
+
