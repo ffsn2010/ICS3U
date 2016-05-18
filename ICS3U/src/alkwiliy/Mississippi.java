da954be6776758e7da5ed95bf125a0e8d7803a54
Faisal Al-kwiliy <faisal.al-kwiliy@hotmail.com>
2016-05-18 14:51:18 -0400

    This program spells out Mississippi

---
+++ ICS3U/src/alkwiliy/Mississippi.java
@@ -0,0 +1,93 @@
+/**
+ * Mississippi.java
+ * This program uses 5 methods including the main method that prints out the word Mississippi
+ * Faisal Al-kwiliy
+ * Tuesday, May 17th, 2016
+ */
+package alkwiliy;
+
+
+public class Mississippi {
+
+	/**
+	 * This method is the main method that prints out Mississippi
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		drawM(); 
+		drawI(); 
+		drawS();
+		drawS();
+		drawI();
+		drawS();
+		drawS();
+		drawI();
+		drawP();
+		drawP();
+		drawI();
+
+	}
+	/**
+	 * This method is the main method that spells out M
+	 * @param args
+	 */
+	public static void drawM()  { // draws M
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M MMM M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("");
+		System.out.println("");
+	}
+	/**
+	 * This method is the main method that spells out I
+	 * @param args
+	 */
+	public static void drawI() { // draws I
+		System.out.println("IIIIIII");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("IIIIIII");
+		System.out.println("");
+		System.out.println("");
+	}
+	/**
+	 * This method is the main method that spells out S
+	 * @param args
+	 */
+	public static void drawS() { // draws S
+		System.out.println(" SSSSSS ");
+		System.out.println("S      S");
+		System.out.println("S       ");
+		System.out.println(" SSSSSS ");
+		System.out.println("       S");
+		System.out.println("S      S");	
+		System.out.println(" SSSSSS ");
+		System.out.println("");
+		System.out.println("");
+	}
+	/**
+	 * This method is the method that spells out P
+	 * @param args
+	 */
+	public static void drawP() { // draws P
+		System.out.println("PPPPPP ");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP ");
+		System.out.println("P      ");
+		System.out.println("P      ");	
+		System.out.println("P      ");
+		System.out.println("");
+		System.out.println("");
+	}
+
+	}
+
+
